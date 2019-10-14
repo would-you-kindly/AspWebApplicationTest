5222f067cd1bcf1c40fa6a9cbb8a68fb9803b73e
ashchelkunov <ashchelkunov@ICS-KK13734>
2019-10-14 15:45:05 +0500

    Еще тест сервиса

---
+++ WebServiceRest/IService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WebServiceRest
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebGet]
+        string EchoWithGet(string s);
+
+        [OperationContract]
+        [WebInvoke]
+        string EchoWithPost(string s);
+    }
+}
---
+++ WebServiceRest/Service.svc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WebServiceRest
+{
+    public class Service : IService
+    {
+        public string EchoWithGet(string s)
+        {
+            return "You said " + s;
+        }
+
+        public string EchoWithPost(string s)
+        {
+            return "You said " + s;
+        }
+    }
+}
